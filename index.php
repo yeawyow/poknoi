231006e7e313a34c72d5e9114ce8efc900bca7f4
DESKTOP-HAQSS2E\yeawnote <yeawyow@gmail.com>
2017-05-26 13:55:45 +0700

    ล่าสุด

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php
 
-    session_start();
-
-
-print_r($_GET);
-$a_get = explode(":", $_GET['r']);
-print_r($a_get);
-
-?>
 <!DOCTYPE html>
 <html lang="th">
     <head>
@@ -67,7 +58,7 @@ print_r($a_get);
 
                             <div class="headerNavigation_lg">
                                 <ul class="list-inline">
-                                    <li><a class="txtautosize hover_unterline" href=""><span class="fa fa-home"></span> หน้าแรก</a></li>
+                                    <li><a class="txtautosize hover_unterline" href="index.php"><span class="fa fa-home"></span> หน้าแรก</a></li>
                                     <li><a class="txtautosize hover_unterline _blank" href=""><span class="fa fa-address-book"></span> รับเรื่องร้องเรียน</a></li>
                                     <li id="menuLogin"><a class="txtautosize hover_unterline _blank" href="#"><span class="fa fa-sign-in"></span> เข้าสู่ระบบ</a></li>
 
@@ -82,9 +73,11 @@ print_r($a_get);
                     <div class="dropdown">
                         <button class="dropbtn">เกี่ยวกับหน่วยงาน</button>
                         <div class="dropdown-content">
-                            <a href="#">ประวัติความเป็นมา</a>
-                            <a href="#">วิสัยทัศน์ พันธกิจ</a>
-                            <a href="#">สภาพข้อมูลพื้นฐาน</a>
+                            <a href="?m=main&p=history">ประวัติความเป็นมา</a>
+                            <a href="?m=main&p=vision">วิสัยทัศน์ พันธกิจ</a>
+                            <a href="?m=main&p=location">สภาพข้อมูลพื้นฐาน</a>
+                             <a href="?m=main&p=charac">อำนาจหน้าที่</a>
+                              <a href="?m=main&p=budget">งบประมาณ</a>
                         </div>
                     </div>
                     <div class="dropdown">
@@ -109,7 +102,17 @@ print_r($a_get);
             </div>
         </header><!-- /header -->
         <div class="containner">
-            <div id="data"></div>
+               <?php
+// Application 
+                $dir = (isset($_GET['m']) ? $_GET['m'] : 'main');
+                $file = (isset($_GET['p']) ? $_GET['p'] : 'dashboard1');
+
+                if (file_exists('modules/' . $dir . '/' . $file . '.php')) {
+                    include 'modules/' . $dir . '/' . $file . '.php';
+                } else {
+                    echo '404,ไม่พบหน้าที่ท่านเรียก';
+                }
+                ?>
             </div> <!-- /container -->
             <div >
                 <div class="section-contact-wrap">
@@ -132,13 +135,13 @@ print_r($a_get);
                         </div>
                     </div>
 
-                    <div class="section-contact-in2">
+                  <!--  <div class="section-contact-in2">
                         <div class="container">
                             <div class="row">
 
                             </div>
                         </div>
-                    </div>
+                    </div>-->
                     <div class="footerWrap">
                         <div class="container">
                             <div class="row">
@@ -161,41 +164,6 @@ print_r($a_get);
 
             </div>
 
-            <!-- Modal -->
-            <div class="modal" id="frmLogin" role="dialog">
-                <div class="modal-dialog">
-
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Login</h4>
-                        </div>
-                        <div class="modal-body">
-                            <h2 class="form-signin-heading">Please sign in</h2>
-                            <div class="form-group">
-                                <label for="username" class="sr-only">Email address</label>
-                                <input type="text" id="username" class="form-control" placeholder="Username" required autofocus>
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="sr-only">Password</label>
-                                <input type="password" id="password" class="form-control" placeholder="Password" required>
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" value="remember-me"> Remember me
-                                </label>
-                            </div>
-                            <button id="btnLogin" class="btn btn-lg btn-primary btn-block" type="button">Sign in</button>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
             <script src="script/jquery/jquery-2.2.4.min.js"></script>
             <script src="script/jquery.js"></script>
             <script src="script/jquery-ui.js"></script>
@@ -205,366 +173,5 @@ print_r($a_get);
             <script src="script/main.js"></script>
             <script src="script/index.js"></script>
 
-
-            
-        <script>
-            var myPopState = false;
-            var rows_note_alert =<?php echo $rows_note_alert; ?>;
-            window_title=getTitle();
-            window.onpopstate = function (event) {
-                //alert(myPopState);
-                if (myPopState == false) {
-                    var now_url = window.location.href;
-                    if (now_url.substr(now_url.length - 1) == '#') {
-                        window.history.replaceState('', '', now_url.substr(0, now_url.length - 1));
-                    } else {
-                        location.reload();
-                    }
-                }
-                myPopState = false;
-                //alert(myPopState);
-            }
-						function loadSidebar(){
-//							alert('dddddddddd');
-							loadAjax("sidebar", "sidebar", "", false,'',false);						}
-            $(function () {
-								
-								setInterval(function(){ loadSidebar(); }, 3000);
-
-                loadSidebar();
-                loadAjax("menu", "menu", "", false);
-                if (rows_note_alert > 0) {
-<?php
-if ($_SESSION['refresh_count'] == 1) {
-    ?>
-                        loadPopup("note_alert_show", "", "ข่าวประกาศ");
-    <?php }
-?>
-                }
-                $(document).bind('fscreenchange', function (e, state, elem) {
-                    // if we currently in fullscreen mode
-                    if ($.fullscreen.isFullScreen()) {
-                    }
-                    $('#state').text($.fullscreen.isFullScreen() ? '' : 'not');
-                });
-                $(window).on('resize', function () {
-                    var max_height = $(window).height() - $("#navTopBarMenu").height();
-                    $("#sidebar").height(max_height + "px");
-                    $("#data").height(max_height + "px");
-
-//                    $("#sidebarMenu").width($("#sidebar").width() + "px");
-
-                });
-<?php
-if (isset($_GET['r'])) {
-    ?>
-                    loadAjax("<?php echo $a_get[0]; ?>", "<?php echo $a_get[2]; ?>", "<?php echo $a_get[1]; ?>", false);
-    <?php
-} else {
-    ?>
-                    loadDefault();
-    <?php
-}
-?>
-
-            });
-            function loadAjax(phpFileName, divTarget, params, pushState, callback="", loading=true) {
-							myPopState = pushState;
-							ajax_session='';
-							var url = "../main/" + phpFileName + ".php";
-							if (loading==true)
-							{
-								loadingShow();
-							}
-							$.ajax({
-								type: "POST",
-								url: url,
-								cache: false,
-								data: {params},
-								success: function (msg) {
-									if (msg.trim().substring(0,15)=="session expired")
-									{
-										alert(url+"loadAjax โปรแกรมหลุดจากการเชื่อมต่อ โปรดล็อคอินใหม่ ");
-										if (php_session=='')
-										{
-											loadingHide();
-											loadDefault();
-										}else{
-											loadDefault();
-											logout();
-										}
-									}else{
-										if (msg.trim().substring(0,13)=="access denied")
-										{
-											loadingHide();
-											alert("คุณไม่มีสิทธิ์ใช้งานหน้านี้");
-										}else{
-											$("#" + divTarget).html(msg);
-											if (php_session==ajax_session){
-												if (pushState === true) {
-														window.history.replaceState('', '', '../main/index.php?r=' + phpFileName + ':' + encodeURIComponent(params) + ':' + divTarget);
-												}
-												if (loading==true)
-												{
-													loadingHide();
-												}
-												if (!(callback === undefined)) {
-													eval(callback);
-												}
-											}else{
-												alert(url+" loadAjax2 โปรแกรมหลุดจากการเชื่อมต่อ โปรดล็อคอินใหม่ ");
-												logout();
-											}
-										}
-									}
-								}
-							});
-            }
-            function loadChart(phpFileName, divTarget, params, pushState, callback) {
-							myPopState = pushState;
-							ajax_session='';
-							var url = "../main/" + phpFileName + ".php";
-							$.ajax({
-								type: "POST",
-								url: url,
-								cache: false,
-								data: {params},
-								success: function (msg) {
-									if (msg.trim().substring(0,15)=="session expired")
-									{
-										alert(url+"loadChart โปรแกรมหลุดจากการเชื่อมต่อ โปรดล็อคอินใหม่ ");
-										if (php_session=='')
-										{
-											loadingHide();
-											loadDefault();
-										}else{
-											loadDefault();
-											logout();
-										}
-									}else{
-										if (msg.trim().substring(0,13)=="access denied")
-										{
-											loadingHide();
-											alert("คุณไม่มีสิทธิ์ใช้งานหน้านี้");
-										}else{
-											$("#" + divTarget).html(msg);
-											if (php_session==ajax_session){
-												if (pushState === true) {
-													window.history.replaceState('', '', '../main/index.php?r=' + phpFileName + ':' + encodeURIComponent(params) + ':' + divTarget);
-												}
-												if (!(callback === undefined)) {
-													eval(callback);
-												}
-											}else{
-												alert("reload "+url);
-												logout();
-											}
-										}
-									}
-								}
-							});
-            }
-
-            function loadDefault() {
-                $("#data").html("");
-		            loadAjax("claim_check", "data", "callback=dashboard1", true);
-//                loadAjax("dashboard1", "data", "", true);
-            }
-            ;
-            function loadPDF(url, header) {
-                $("#iframePDF").attr("src", url);
-                $("#iframePDFTitle").html(header);
-                $("#ModalPrintPDF").modal();
-            }
-            function loadPopup(url, params, header) {
-                loadAjax(url, "popupData", params, false);
-                $("#popupHeader").html(header);
-                $("#modalPopup").modal();
-            }
-            function checkRequired(formName,a_field) {
-                var check_return = false;
-                var a_alert = [];
-                var a_alert_field = [];
-                var a_alert_i = 0;
-                jQuery.each(a_field, function (i, val) {
-                    if ($("#"+formName).find("#" + val).val() == '') {
-                        ++a_alert_i;
-                        a_alert.push("&nbsp;&nbsp;&nbsp;" + a_alert_i + ". " + $("#"+formName).find("#" + val).parent().children("label").html());
-                        a_alert_field.push(val);
-                    }
-                });
-                if (a_alert.length > 0) {
-                    bootbox.alert({
-                        message: "คุณยังไม่ป้อนข้อมูลดังต่อไปนี้ <br>" + a_alert.join("<br>"),
-                        size: 'small',
-                        callback: function () {
-                            $("#"+formName).find("#" + a_alert_field[0]).focus();
-                        }
-                    });
-
-                } else {
-                    check_return = true;
-                }
-                return check_return;
-            }
-
-            function chartFromTable(target, source, chartType, titleText) {
-                Highcharts.chart(target, {
-                    data: {
-                        table: source
-                    },
-                    chart: {
-                        type: chartType
-                    },
-                    title: {
-                        text: titleText
-                    },
-                    yAxis: {
-                        allowDecimals: false,
-                        title: {
-                            text: 'Units'
-                        }
-                    },
-                    tooltip: {
-                        formatter: function () {
-                            return '<b>' + this.series.name + '</b><br/>' +
-                                    this.point.y + ' ' + this.point.name.toLowerCase();
-                        }
-                    }
-                });
-            }
-            function modal_set_size(modal_size) {
-                if (!(modal_size === undefined)) {
-                    $("#modal").children().first().addClass(modal_size);
-                } else {
-                    $("#modal").children().first().removeClass("modal-lg");
-                    $("#modal").children().first().removeClass("modal-sm");
-                }
-            }
-						function loadingShow(){
-							bootbox.dialog({ 
-								message: '<div class="text-center"><i class="fa fa-spin fa-spinner"></i> Loading...</div>' 
-							});						
-						}
-						function loadingHide(){
-							$('.bootbox.modal').modal('hide');
-						}
-						function strToDate(bbbbmmdd=''){
-							if (bbbbmmdd.trim()!=''){
-								var bbbb=bbbbmmdd.substring(0,4);
-								var mm=bbbbmmdd.substring(4,6);
-								var dd=bbbbmmdd.substring(6,8);
-								var strMonthCut =["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค."];
-								var strMonthThai=strMonthCut[mm*1];
-								return (dd*1)+" "+strMonthThai+" "+bbbb;
-							}else{
-								return '';
-							}
-						}
-						function eStrToDate(YYYY_mm_dd=''){
-							if (YYYY_mm_dd.trim()!=''){
-								var YYYY=YYYY_mm_dd.substring(0,4);
-								var mm=YYYY_mm_dd.substring(6,7);
-								var dd=YYYY_mm_dd.substring(8,10);
-								var strMonthCut =["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค."];
-								var strMonthThai=strMonthCut[mm*1];
-								return (dd*1)+" "+strMonthThai+" "+(parseInt(YYYY)+543);
-							}else{
-								return '';
-							}
-						}
-
- function getNowDateTime() {
-    var now     = new Date(); 
-    var year    = now.getFullYear();
-    var month   = now.getMonth()+1; 
-    var day     = now.getDate();
-    var hour    = now.getHours();
-    var minute  = now.getMinutes();
-    var second  = now.getSeconds(); 
-    if(month.toString().length == 1) {
-        var month = '0'+month;
-    }
-    if(day.toString().length == 1) {
-        var day = '0'+day;
-    }   
-    if(hour.toString().length == 1) {
-        var hour = '0'+hour;
-    }
-    if(minute.toString().length == 1) {
-        var minute = '0'+minute;
-    }
-    if(second.toString().length == 1) {
-        var second = '0'+second;
-    }   
-    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
-     return dateTime;
-}
-
-   function number_format (number, decimals, dec_point, thousands_sep) {
-      var exponent = "";
-      var numberstr = number.toString ();
-      var eindex = numberstr.indexOf ("e");
-      if (eindex > -1) {
-         exponent = numberstr.substring (eindex);
-         number = parseFloat (numberstr.substring (0, eindex));
-      }
-      if (decimals != null) {
-         var temp = Math.pow (10, decimals);
-         number = Math.round (number * temp) / temp;
-      }
-      var sign = number < 0 ? "-" : "";
-      var integer = (number > 0 ? Math.floor (number) : Math.abs (Math.ceil (number))).toString ();
-      var fractional = number.toString ().substring (integer.length + sign.length);
-      dec_point = dec_point != null ? dec_point : ".";
-      fractional = decimals != null && decimals > 0 || fractional.length > 1 ? (dec_point + fractional.substring (1)) : "";
-      if (decimals != null && decimals > 0) {
-         for (i = fractional.length - 1, z = decimals; i < z; ++i) {
-            fractional += "0";
-         }
-      }
-      thousands_sep = (thousands_sep != dec_point || fractional.length == 0) ? thousands_sep : null;
-      if (thousands_sep != null && thousands_sep != "") {
-         for (i = integer.length - 3; i > 0; i -= 3){
-            integer = integer.substring (0 , i) + thousands_sep + integer.substring (i);
-         }
-      }
-      return sign + integer + fractional + exponent;
-   }
-
-        function logout() {
-            $.ajax({
-                method: "POST",
-                url: "logout.php"
-            })
-                    .done(function (msg) {
-                        if (msg == 'Ok') {
-													loadDefault();
-                            location.reload();
-//                            loadAjax("menu", "menu");
-//                            loadDefault();
-                        }
-                    });
-        }
-        function update() {
-            $.ajax({
-                method: "POST",
-                url: "../update/update_refresh.php"
-            })
-                    .done(function (msg) {
-                        if (msg == 'Ok') {
-                            location.reload();
-                        }
-                    });
-        }
-        function getTitle() {
-           return $('title').html();
-        }
-
-        function setTitle(_title) {
-            $('title').html(_title);
-        }
-
-        </script>
     </body>
 </html>
---
+++ modules/main/vision.php
@@ -0,0 +1,40 @@
+<div class="container" style="background-color:#ffffff"><h1><strong>วิสัยทัศน์ (Vision)</strong></h1>
+
+<p>&nbsp; &nbsp;<strong>&ldquo;พอกน้อยน่าอยู่&nbsp;</strong></p>
+
+<p><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;มุ่งสู่นวัตกรรม&nbsp;</strong></p>
+
+<p><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;นำเศรษฐกิจพอเพียง</strong></p>
+
+<p><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;รองรับ</strong>&nbsp;<strong>AEC</strong>&nbsp;<strong>ประชาชนมีส่วนร่วม</strong><strong>&rdquo;</strong></p>
+
+<hr />
+<h1>พันธกิจ (Mission)</h1>
+
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<strong>1. จัดให้มีและบำรุงรักษาโครงสร้างพื้นฐานทางบก ทางน้ำ และไฟฟ้า</strong></p>
+
+<p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2. ส่งเสริมและพัฒนาคุณภาพชีวิต และพัฒนาการศึกษาของประชาชน</strong></p>
+
+<p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 3. ส่งเสริมและเสริมสร้างการจัดระเบียบชุมชน สังคมและความสงบเรียบร้อย</strong></p>
+
+<p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 4. ส่งเสริมและเสริมสร้างการค้า การลงทุน พาณิชยกรรม เกษตรกรรม</strong></p>
+
+<p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 5. ส่งเสริมและสนับสนุนการท่องเที่ยว ประเพณี ศิลปะ วัฒนธรรมและภูมิปัญญาท้องถิ่น</strong></p>
+
+<p><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;6.&nbsp;ส่งเสริมและเสริมสร้างการบริหารจัดการทรัพยากรธรรมชาติและสิ่งแวดล้อม</strong></p>
+
+<p><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;7. ส่งเสริมและสร้างเสริมสุขภาพ ป้องกันโรค เพื่อมุ่งสู่ตำบลสุขภาวะ</strong></p>
+
+<p><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;8. เสริมสร้างความรู้รักสามัคคี และเสริมสร้างความมั่นคงของชาติ</strong></p>
+
+<hr /> </div>
+                </header>
+
+            
+
+        </div>
+    </div>
+    
+</div> 
+
+               
