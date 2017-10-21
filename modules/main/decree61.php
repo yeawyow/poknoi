63e64aba4881038b138e8f4466ff254c1c2f0136
yeawyow <yeawyow@gmail.com>
2017-10-21 20:05:29 +0700

    เทศบัญญัติ61

--- modules/main/decree61.php
+++ modules/main/decree61.php
@@ -1,7 +1,138 @@
 
 <div class="container" style="background-color:#ffffff;"><h2>เทศบัญญัติปี 2561</h2>
-เทศบาลตำบลพอกน้อย เป็นองค์กรปกครองส่วนท้องถิ่นรูปแบบหนึ่ง เปลี่ยนแปลงจากสภาตำบล เป็นองค์การบริหารส่วนตำบลพอกน้อย เมื่อวันที่ 23 กุมภาพันธ์ 2540 ตามประกาศกระทรวงมหาดไทยลงวันที่ 16 ธันวาคม 2539 และยกฐานะเป็นเทศบาลตำบลเมื่อวันที่ 12 ตุลาคม 2552 ตามประกาศกระทรวงมหาดไทยลงวันที่ 12 ตุลาคม 2552 มีสำนักงานตั้งอยู่ที่บ้านพอกน้อยพัฒนา หมู่ที่ 8 ตำบลพอกน้อย  อำเภอพรรณานิคม  จังหวัดสกลนคร
-<br><img src="img/pok1.jpg"><br>
-ปัจจุบันเทศบาลตำบลพอกน้อย ตั้งอยู่ที่ ถ.นิตโย ต.พอกน้อย อ.พรรณานิคม จ.สกลนคร โดยห่างจากตัวจังหวัดสกลนครไปทางทิศตะวันตก ประมาณ 25 ก.ม
+
+    <div id="exTab1">	
+        <ul  class="nav nav-pills">
+            <li class="active">
+                <a  href="#1a" data-toggle="tab">คำแถลงการ</a>
+            </li>
+            <li><a href="#2a" data-toggle="tab">รายงานประมาณการรายจ่าย</a>
+            </li>
+            <li><a href="#3a" data-toggle="tab">รายงานประมาณการรายรับ</a>
+            </li>
+            <li><a href="#4a" data-toggle="tab">รายจ่าย</a>
+            </li>
+             <li><a href="#5a" data-toggle="tab">รายรับ</a>
+            </li>
+            <li><a href="#6a" data-toggle="tab">รายละเอียดประมาณการรายจ่าย</a>
+            </li>
+             <li><a href="#7a" data-toggle="tab">รายละเอียดประมาณการรายรับ</a>
+            </li>
+        </ul>
+
+        <div class="tab-content clearfix">
+            <div class="tab-pane active" id="1a">
+                <h3>คำแถลงการ</h3>
+                <div id="example1">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/คำแถลงการ61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/คำแถลงการ61.pdf", "#example1", {width: "100%", height: "600px"});
+
+
+                </script>
+
+            </div>
+            <div class="tab-pane" id="2a">
+                <h3>รายงานประมาณการรายจ่าย</h3>
+                <div id="example12">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/รายงานประมาณการรายจ่าย61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+                <script type="text/javascript" src="includes/pdfobject.min.js"></script>
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/รายงานประมาณการรายจ่าย61.pdf", "#example12", {width: "100%", height: "600px"});
+
+
+                </script>
+            </div>
+            <div class="tab-pane" id="3a">
+                <h3>รายงานประมาณการรายรับ</h3>
+                <div id="example13">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/รายงานประมาณการรายรับ61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/รายงานประมาณการรายรับ61.pdf", "#example13", {width: "100%", height: "600px"});
+
+
+                </script>
+
+            </div>
+            <div class="tab-pane" id="4a">
+                <h3>รายจ่าย</h3>
+                <div id="example14">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/รายจ่าย61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/รายจ่าย61.pdf", "#example14", {width: "100%", height: "600px"});
+
+
+                </script>
+            </div>
+              <div class="tab-pane" id="5a">
+                <h3>รายรับ</h3>
+                <div id="example15">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/รายรับ61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/รายรับ61.pdf", "#example15", {width: "100%", height: "600px"});
+
+
+                </script>
+            </div>
+            
+             <div class="tab-pane" id="6a">
+                <h3>ายละเอียดประมาณการรายจ่าย</h3>
+                <div id="example16">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/รายละเอียดประมาณการรายจ่าย61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/รายละเอียดประมาณการรายจ่าย61.pdf", "#example16", {width: "100%", height: "600px"});
+
+
+                </script>
+            </div>
+            
+              <div class="tab-pane" id="7a">
+                <h3>ายละเอียดประมาณการรายรับ</h3>
+                <div id="example17">
+                    ไม่ได้ติดตั้งโปรแกรม Adobe Reader หรือบราวเซอร์ไม่รองรับการแสดงผล PDF 
+                    <a href="modules/decree_pdf/รายละเอียดประมาณการรายรับ61.pdf">คลิกที่นี้เพื่อดาวน์โหลดไฟล์ PDF</a>
+                </div>
+
+
+                <script>
+
+                    PDFObject.embed("modules/decree_pdf/รายละเอียดประมาณการรายรับ61.pdf", "#example17", {width: "100%", height: "600px"});
+
+
+                </script>
+            </div>
+        </div>
+    </div>
+
+
+    <hr></hr>
+
+
 </div>
-<br>
